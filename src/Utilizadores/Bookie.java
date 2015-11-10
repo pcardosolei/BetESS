08f415049878fac2c91ee090bb0db2f3928e4c07
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2015-11-10 18:57:08 +0000

    cenas 1

--- src/Utilizadores/Bookie.java
+++ src/Utilizadores/Bookie.java
@@ -16,10 +16,16 @@ import java.util.Observer;
 public class Bookie extends Utilizador implements Observer {
     
     
-    public Bookie(String nome, String email){
-        super(nome,email);
+    public Bookie(String nome, String email,String password){
+        super(nome,email,password);
     }
 
+    
+    public boolean verificaUtilizador(String nome,String password){
+        return super.verificaUtilizador(nome, password);
+    }
+    
+    
     @Override
     public void update(Observable o, Object arg) {
         if(o!=null)
